45ce30cbfbdb47e33eaafb3d9382ca769a7a05c3
PaulKrznaric <paulkrznaric@gmail.com>
2022-05-18 18:22:27 -0700

    Added terraform EC2 deployment code

---
+++ infrastructure/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.27"
+    }
+  }
+
+  required_version = ">=0.14.9"
+}
+
+provider "aws" {
+  profile = "emrdev"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "db_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleDBDeployment"
+  }
+}
+
+resource "" "name" {
+
+}
