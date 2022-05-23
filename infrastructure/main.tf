7c2cdcf899fdfbfbc079427a8c3f471937da2a8f
PaulKrznaric <paulkrznaric@gmail.com>
2022-05-22 17:45:34 -0700

    Attempt to execute psql files against the remote database in terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -28,7 +28,7 @@ resource "aws_db_instance" "db_server" {
   allocated_storage   = 20
   identifier          = "dev-db"
   engine              = "postgres"
-  engine_version      = "15"
+  engine_version      = "14"
   instance_class      = "db.t3.micro"
   name                = "pgdb001"
   username            = "devadmin"
@@ -45,4 +45,22 @@ output "db_password" {
   value     = random_password.password.result
   sensitive = true
 }
+
+resource "null_resource" "setup_db" {
+  depends_on = [aws_db_instance.db_server]
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    working_dir = "/tmp"
+    command     = <<-EOT
+            #!/bin/bash
+            sudo yum update -y
+            sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-6-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+            sudo yum install postgresql -y
+            export PGPASSWORD = ${random_password.password.result}
+             psql --host=${aws_db_instance.db_server.endpoint} --port=5432 --username=devadmin --dbname=pgdb001 < setup.psql
+            EOT      
+  }
+}
+
+
 
